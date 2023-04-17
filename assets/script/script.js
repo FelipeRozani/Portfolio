134c77981277ba8c0be5ae634d5eb15c30a40e86
FelipeRozani <felipe.rozani@gmail.com>
2023-04-16 22:11:07 -0300

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,29 @@
+const btnMudaCor = document.querySelector('.btn-muda');
+const img = document.querySelector('.img-btn');
+const corpo = document.querySelector('body');
+const cabecalho = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+const qualificacoes = document.querySelector('.qualificacoes')
+const imgApresentacao = document.querySelector('.img-1')
+
+btnMudaCor.addEventListener('click', () => {
+    corpo.classList.add('body-2');
+    cabecalho.classList.add('header-2');
+    main.classList.add('main-2');
+    footer.classList.add('footer-2');
+    qualificacoes.classList.add('qualificacoes-2');
+    imgApresentacao.setAttribute('src', './assets/img/FotoFelipe-Edit.png');
+    img.setAttribute('src', './assets/img/moon.png')
+});
+
+btnMudaCor.addEventListener('dblclick', () => {
+    corpo.classList.remove('body-2');
+    cabecalho.classList.remove('header-2');
+    img.classList.remove('img-2')
+    main.classList.remove('main-2');
+    footer.classList.remove('footer-2');
+    qualificacoes.classList.remove('qualificacoes-2');
+    imgApresentacao.setAttribute('src', './assets/img/F.png');
+    img.setAttribute('src', './assets/img/sun.png')
+})
